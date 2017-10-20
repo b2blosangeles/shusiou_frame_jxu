417374117afe40823f51d777b77aa71ae1cf6912
b2blosangeles <b2blosangeles@gmail.com>
2017-10-19 17:18:14 -0700

    Update log.js

--- package/log/log.js
+++ package/log/log.js
@@ -5,17 +5,19 @@
 	var obj =  function () {
 		var fs = require('fs'), exec = require('child_process').exec;
 		this.write = function(file, cmd, contents) {
-			var l =  contents.toString().replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '\\$&').
-				replace(/[\n\r]/g, '\\n');
-			var c =  cmd.toString().replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '\\$&').
-				replace(/[\n\r]/g, '\\n');
+			var l =  this.transformText(contents);
+			var c =  this.transformText(cmd);
 			
 			var log_str = "sed -i '1s/^/=== " + c + ' --> ' +  new Date().toString() + '===\\n' + l + "\\n/' " + file;
   			fs.appendFile(file, ' ', function(err) {
 				exec(log_str, function(error, stdout, stderr) {});
   			});			
 			
-		};	
+		};
+		this.transformText = function(contents) {
+			return contents.toString().replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '\\$&').
+				replace(/[\n\r]/g, '\\n')
+		}
 	};
 	module.exports = obj;
 })();
