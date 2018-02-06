945c646270ad3c0714ba1eb4c4c23af1844cb6fd
b2blosangeles <b2blosangeles@gmail.com>
2018-02-06 10:33:17 -0800

    Update q_dns.js

--- q_dns.js
+++ q_dns.js
@@ -76,10 +76,12 @@ function getServerIP() {
     }
     return address;
 };
+/*
 var dnsd = require('./package/dnsd/node_modules/dnsd');
 dnsd.createServer(function(req, res) {
   res.end('1.2.3.4')
 }).listen(dnsport, ['127.0.0.1']);
 console.log('Server running at 127.0.0.1:5353');
+*/
 console.log(getServerIP());
 
