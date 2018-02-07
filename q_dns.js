4d08dc116268ca65ba5eb6e9ada0a1f0c408deca
b2blosangeles <b2blosangeles@gmail.com>
2018-02-07 08:12:12 -0800

    Update q_dns.js

--- q_dns.js
+++ q_dns.js
@@ -86,7 +86,7 @@ dns.lookup('ns1.shusiou.win', (err, address, family) => {
 		let dnsd = require('./package/dnsd/node_modules/dnsd');
 		try {
 			dnsd.createServer(function(req, res) {
-				console.log(req);
+				console.log(req.question);
 				res.end('67.205.189.126');
 			}).listen(dnsport, address)
 			console.log('DNS Server running at ' + address + ':' + dnsport);
