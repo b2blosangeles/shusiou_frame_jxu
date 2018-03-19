c089fd3cd0c53c5f68cae33bc121117a84e74534
b2blosangeles <b2blosangeles@gmail.com>
2018-03-19 08:26:09 -0700

    Update cron_service.js

--- cron/cron_service.js
+++ cron/cron_service.js
@@ -1,5 +1,5 @@
 var CronJobManager = require('./crontab_manager.js');
-var crowdProcess = require('./package/crowdProcess/crowdProcess');
+var CrowdProcess = require('./package/crowdProcess/crowdProcess');
 var manager = new CronJobManager();
 var exec = require('child_process').exec;
 var fs    = require('fs'), path = require('path');
